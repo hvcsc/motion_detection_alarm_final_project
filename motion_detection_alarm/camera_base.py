8fad6d5e1cf70377119803b6fd3ffbfe40f87659
hvcsc <jrhcmll13@gmail.com>
2025-06-08 23:11:25 +0800

    import the opencv and initialize the camera

--- motion_detection_alarm/camera_base.py
+++ motion_detection_alarm/camera_base.py
@@ -1,6 +1,13 @@
 #import the OpenCV library for accessing the webcam and image processing
-#initialize the camera
-#set the height and width of the video frame
+import cv2
+
+class Camera:
+    def __init__(self, width = 640, height = 480):
+        #initialize the camera
+        self._cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+        #set the height and width of the video frame
+
 #read a single frame from the camera
 #return the captured frame
 #release the camera resource to free it for other application
