ad1607beef7b97520d8216eec8284958f832e921
hvcsc <jrhcmll13@gmail.com>
2025-06-15 09:53:15 +0800

    resize the frame, convert color, and apply gaussian blur

--- motion_detection_alarm/frame_processor.py
+++ motion_detection_alarm/frame_processor.py
@@ -2,7 +2,13 @@
 import cv2
 import imutils
 
-#resize the frame
-#convert the color frame to grayscale for easier processing
-#apply gaussian blur
-#return processed frame
+class FrameProcessor:
+    def preprocess(self, frame):
+        #resize the frame
+        frame = imutils.resize(frame, width = 500)
+        #convert the color frame to grayscale for easier processing
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #apply gaussian blur
+        frame = cv2.GaussianBlur(frame, (5, 5), 0)
+
+        #return processed frame
