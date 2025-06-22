ef62e02dbf174bc38905706d37fa4c7e42b934b5
hvcsc <jrhcmll13@gmail.com>
2025-06-22 22:49:16 +0800

    abstract base class for motion detection and methods to be overridden

--- motion_detection_alarm/motion_detector.py
+++ motion_detection_alarm/motion_detector.py
@@ -1,4 +1,10 @@
 #import OpenCV
+import cv2
+
 #abstract base class for motion detection
-    #abstract methods to be overridden
+class BaseMotionDetector:
+    def detect(self, current_frame, reference_frame):
+        #abstract methods to be overridden
+        raise NotImplementedError("This method should be overridden.")
+
 #concrete implementation using basic frame differencing
