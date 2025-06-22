7c6a11c866a56f7ff84bb5cf4ba36c35634c685f
hvcsc <jrhcmll13@gmail.com>
2025-06-23 00:11:23 +0800

    pseudocode

---
+++ motion_detection_alarm/alarm_manager.py
@@ -0,0 +1,6 @@
+#import threading and winsound
+#initialize variables
+#enable or disable alarm mode
+#start the alarm sound if it's not already active
+#play beeping sound unless turned off
+#property to access the currect alarm mode
