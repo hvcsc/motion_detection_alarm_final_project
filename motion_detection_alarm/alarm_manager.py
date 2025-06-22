de02e96b6b30ed3926b878b71a743c8ce34dd7af
hvcsc <jrhcmll13@gmail.com>
2025-06-23 00:19:59 +0800

    import files and initialize the variables

--- motion_detection_alarm/alarm_manager.py
+++ motion_detection_alarm/alarm_manager.py
@@ -1,6 +1,14 @@
 #import threading and winsound
-#initialize variables
-#enable or disable alarm mode
-#start the alarm sound if it's not already active
-#play beeping sound unless turned off
-#property to access the currect alarm mode
+import threading
+import winsound
+
+class AlarmManager:
+    #initialize variables
+    def __init__(self):
+        self._alarm_active = False
+        self._alarm_mode = False
+        
+    #enable or disable alarm mode
+    #start the alarm sound if it's not already active
+    #play beeping sound unless turned off
+    #property to access the current alarm mode
