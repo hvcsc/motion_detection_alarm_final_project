81185bc98609b6cc5a7f9a0b87ef0ba61139446b
hvcsc <jrhcmll13@gmail.com>
2025-06-23 00:31:20 +0800

    create and instance of the motion detection system and start

--- motion_detection_alarm/main.py
+++ motion_detection_alarm/main.py
@@ -1,3 +1,9 @@
 #import main class
+from motion_detector import MotionDetector
+
 #create and instance of the motion detection system and start
+def main():
+    detector = MotionDetector()
+    detector.start()
+
 #run the main function
